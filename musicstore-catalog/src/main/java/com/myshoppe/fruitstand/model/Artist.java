01563219bed271b28ffb2eb4395eebfdd2ab28b6
scouring pad <hcc780921@gmail.com>
2022-08-03 09:18:38 -0700

    Adding Artist model contents in catalog

--- musicstore-catalog/src/main/java/com/myshoppe/fruitstand/model/Artist.java
+++ musicstore-catalog/src/main/java/com/myshoppe/fruitstand/model/Artist.java
@@ -1,6 +1,7 @@
 package com.myshoppe.fruitstand.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "artist")
@@ -15,5 +16,68 @@ public class Artist {
     private String twitter;
 
 
+    public Artist() {
+    }
 
+    public Artist(Integer id, String name, String instagram, String twitter) {
+        this.id = id;
+        this.name = name;
+        this.instagram = instagram;
+        this.twitter = twitter;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInstagram() {
+        return instagram;
+    }
+
+    public void setInstagram(String instagram) {
+        this.instagram = instagram;
+    }
+
+    public String getTwitter() {
+        return twitter;
+    }
+
+    public void setTwitter(String twitter) {
+        this.twitter = twitter;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Artist artist = (Artist) o;
+        return Objects.equals(id, artist.id) && Objects.equals(name, artist.name) && Objects.equals(instagram, artist.instagram) && Objects.equals(twitter, artist.twitter);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, instagram, twitter);
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", instagram='" + instagram + '\'' +
+                ", twitter='" + twitter + '\'' +
+                '}';
+    }
 }
