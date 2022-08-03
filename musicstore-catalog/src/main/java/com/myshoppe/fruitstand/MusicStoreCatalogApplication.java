0526383a6ba3c68043d22c8d5d28a22ef6e0d204
scouring pad <hcc780921@gmail.com>
2022-08-03 09:37:46 -0700

    Adding track model contents in catalog

--- musicstore-catalog/src/main/java/com/myshoppe/fruitstand/MusicStoreCatalogApplication.java
+++ musicstore-catalog/src/main/java/com/myshoppe/fruitstand/MusicStoreCatalogApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FruitStandApplication {
+public class MusicStoreCatalogApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FruitStandApplication.class, args);
+		SpringApplication.run(MusicStoreCatalogApplication.class, args);
 	}
 
 }
