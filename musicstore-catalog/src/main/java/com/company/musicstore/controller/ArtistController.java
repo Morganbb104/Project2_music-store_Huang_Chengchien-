4e2078bfbcefce9ffa5842b2cf1f51468b48e849
scouring pad <hcc780921@gmail.com>
2022-08-03 13:01:22 -0700

    adding CRUD in Artist controller

--- musicstore-catalog/src/main/java/com/company/musicstore/controller/ArtistController.java
+++ musicstore-catalog/src/main/java/com/company/musicstore/controller/ArtistController.java
@@ -1,9 +1,53 @@
 package com.company.musicstore.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.company.musicstore.model.Artist;
+import com.company.musicstore.model.Label;
+import com.company.musicstore.repository.ArtistRepository;
+import com.company.musicstore.repository.LabelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/artist")
 public class ArtistController {
+
+    @Autowired
+    private ArtistRepository repo;
+    @GetMapping("/artist")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Artist> getAllLabel() {
+        return repo.findAll();
+    }
+
+    @GetMapping("/artist/{id}")
+    public Artist getArtistById(@PathVariable int id) {
+        Optional<Artist> returnVal = repo.findById(id);
+        if (returnVal.isPresent()) {
+            return returnVal.get();
+        } else {
+            return null;
+        }
+    }
+
+    @PutMapping("/artist/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateArtist(@RequestBody Artist artist) {
+        repo.save(artist);
+    }
+
+    @PostMapping(value="/artist")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Artist createArtist(@RequestBody Artist artist) {
+        return repo.save(artist);
+    }
+
+    @DeleteMapping("artist/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeArtist(@PathVariable int id) {
+        repo.deleteById(id);
+    }
+
 }
--- musicstore-catalog/src/main/java/com/company/musicstore/controller/TrackController.java
+++ musicstore-catalog/src/main/java/com/company/musicstore/controller/TrackController.java
@@ -1,9 +1,30 @@
 package com.company.musicstore.controller;
 
+
+
+import com.company.musicstore.model.Track;
+import com.company.musicstore.repository.TrackRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/track")
 public class TrackController {
+
+    @Autowired
+    private TrackRepository repo;
+
+    @GetMapping("/track")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Track> getAllTrack() {
+        return repo.findAll();
+    }
+
+
+
 }
