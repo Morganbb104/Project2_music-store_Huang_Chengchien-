2507bd0ad7b6e291df72be0655580f875ae2b61b
scouring pad <hcc780921@gmail.com>
2022-08-03 12:29:54 -0700

    adding CRUD in controller

--- musicstore-catalog/src/main/java/com/company/musicstore/controller/LabelController.java
+++ musicstore-catalog/src/main/java/com/company/musicstore/controller/LabelController.java
@@ -6,12 +6,10 @@ import com.company.musicstore.model.Produce;
 import com.company.musicstore.repository.LabelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class LabelController {
@@ -21,6 +19,34 @@ public class LabelController {
     @ResponseStatus(HttpStatus.OK)
     public List<Label> getAllLabel() {
         return repo.findAll();
+    }
+
+    @GetMapping("/label/{id}")
+    public Label getLabelById(@PathVariable int id) {
+        Optional<Label> returnVal = repo.findById(id);
+        if (returnVal.isPresent()) {
+            return returnVal.get();
+        } else {
+            return null;
+        }
+    }
+
+    @PutMapping("/label/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateLabel(@RequestBody Label label) {
+        repo.save(label);
+    }
+
+    @PostMapping(value="/label")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Label createLabel(@RequestBody Label label) {
+        return repo.save(label);
+    }
+
+    @DeleteMapping("label/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeLabel(@PathVariable int id) {
+        repo.deleteById(id);
     }
 
 }
