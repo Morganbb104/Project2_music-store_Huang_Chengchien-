72c65e8d2b60ce6d00e71e6076132b0269f49914
scouring pad <hcc780921@gmail.com>
2022-08-03 11:31:01 -0700

    editing data.sql in catalog

--- musicstore-catalog/sql/data.sql
+++ musicstore-catalog/sql/data.sql
@@ -11,14 +11,14 @@ insert into label(`name`, website) values
     ('Stone label','www.stone.com');
 
 insert into album(title, artist_id, release_date, label_id, list_price)values
-    ('Fairy tale', '', 2022-08-02, '', 19.99),
-    ('Stone rack', '', 2022-08-01, '', 29.99),
-    ('Andrew star', '', 2022-08-03, '', 39.99);
+    ('Fairy tale', 1, '2022-08-02', 2, 19.99),
+    ('Stone rack', 2, '2022-08-01', 3, 29.99),
+    ('Andrew star',3, '2022-08-03', 1, 39.99);
 
 insert into track(album_id, title, run_time) values
-    ('', 'Mountain Monster', 120),
-    ('', 'Freaking drinks', 130),
-    ('', 'Wonder island', 150),
-    ('', 'Endless ego', 140),
-    ('', 'Best scenario', 100),
-    ('', 'Long meditation', 120);
+    (1, 'Mountain Monster', 120),
+    (2, 'Freaking drinks', 130),
+    (3, 'Wonder island', 150),
+    (1, 'Endless ego', 140),
+    (2, 'Best scenario', 100),
+    (3, 'Long meditation', 120);
--- musicstore-catalog/src/main/java/com/company/musicstore/controller/LabelController.java
+++ musicstore-catalog/src/main/java/com/company/musicstore/controller/LabelController.java
@@ -1,10 +1,28 @@
 package com.company.musicstore.controller;
 
 
+import com.company.musicstore.model.Label;
+import com.company.musicstore.model.Produce;
+import com.company.musicstore.repository.LabelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/label")
 public class LabelController {
+    @Autowired
+    private LabelRepository repo;
+
+    @GetMapping(value="/label")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Label> getAllProduce() {
+        return repo.findAll();
+    }
+
 }
