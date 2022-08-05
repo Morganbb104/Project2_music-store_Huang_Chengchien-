e3fac1b067be2f1b092f7111038da1e92eba3c3f
scouring pad <hcc780921@gmail.com>
2022-08-05 14:00:39 -0700

    LabelRecommendationControllerTest completed

--- musicstore-recommendations/app/src/test/java/com/company/musicstorerecommendations/controller/ArtistRecommendationControllerTest.java
+++ musicstore-recommendations/app/src/test/java/com/company/musicstorerecommendations/controller/ArtistRecommendationControllerTest.java
@@ -1,7 +1,116 @@
 package com.company.musicstorerecommendations.controller;
 
-import static org.junit.Assert.*;
+import com.company.musicstorerecommendations.model.Artist;
+import com.company.musicstorerecommendations.repository.ArtistRecommendationRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ArtistRecommendationController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class ArtistRecommendationControllerTest {
+    @MockBean
+    private ArtistRecommendationRepository repo;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Before
+    public void setUp() throws Exception {
+        setupArtistRepoMock();
+    }
+
+    private void setupArtistRepoMock(){
+//            // Arrange
+        Artist artist = new Artist(3,1,3,true);
+        Artist artistWithoutId = new Artist(1,3,true);
+        List<Artist> artists = Arrays.asList(artist);
+
+        doReturn(artists).when(repo).findAll();
+        doReturn(artist).when(repo).save(artistWithoutId);
+
+    }
+
+    @Test
+    public void getAllArtistShouldReturnAListAnd200() throws Exception {
+        // Arrange
+        Artist artist = new Artist(3,1,3,true);
+        List<Artist> ArtistList = Arrays.asList(artist);
+
+        String expectedJsonValue = mapper.writeValueAsString(ArtistList);
+        // Act
+        mockMvc.perform(get("/artist"))
+                .andDo(print())
+                .andExpect(status().isOk())                 // Assert
+                .andExpect(content().json(expectedJsonValue));
+    }
+
+    @Test
+    public void createAlbumShouldReturnNewAlbumAndStatus201() throws Exception {
+        // Arrange
+        Artist artist = new Artist(3,1,3,true);
+        List<Artist> artistList = Arrays.asList(artist);
+
+        String expectedJsonValue = mapper.writeValueAsString(artistList);
+        mockMvc.perform(get("/artist"))
+                .andDo(print())
+                .andExpect(status().isOk())                 // Assert
+                .andExpect(content().json(expectedJsonValue));
+    }
+
+    @Test
+    public void getArtistByIdShouldReturnStatus200() throws Exception {
+        Artist outputArtist = new Artist(3,1,3,true);
+        String outputArtistJson = mapper.writeValueAsString(outputArtist);
+        doReturn(Optional.of(outputArtist)).when(repo).findById(1);
+        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/artist/1"))
+                .andExpect(status().isOk())
+                .andExpect((content().json(outputArtistJson)));
+    }
+
+    @Test
+    public void updateArtistShouldReturnStatus200() throws Exception {
+        Artist InputArtist = new Artist(3,1,3,true);
+        String expectedArtistJson = mapper.writeValueAsString(InputArtist);
+        mockMvc.perform(put("/artist/1")
+                        .content(expectedArtistJson)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void deleteArtistByIdShouldReturnStatus204() throws Exception {
+        mockMvc.perform(delete("/artist/2"))
+                .andDo(print())          // Assert
+                .andExpect(status().isNoContent());  // Assert
+
+
+    }
 
 }
--- musicstore-recommendations/app/src/test/java/com/company/musicstorerecommendations/controller/LabelRecommendationControllerTest.java
+++ musicstore-recommendations/app/src/test/java/com/company/musicstorerecommendations/controller/LabelRecommendationControllerTest.java
@@ -1,7 +1,121 @@
 package com.company.musicstorerecommendations.controller;
 
-import static org.junit.Assert.*;
+import com.company.musicstorerecommendations.model.Label;
+import com.company.musicstorerecommendations.repository.LabelRecommendationRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(LabelRecommendationController.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class LabelRecommendationControllerTest {
 
+    @MockBean
+    private LabelRecommendationRepository repo;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper mapper = new ObjectMapper();
+
+
+    @Before
+    public void setUp() throws Exception {
+        setupLabelRepoMock();
+    }
+
+    private void setupLabelRepoMock() {
+        Label label = new Label(1,1,1,true);
+        Label labelWithoutId = new Label(1,1,true);
+        List<Label> labelList = Arrays.asList(label);
+
+        doReturn(labelList).when(repo).findAll();
+        doReturn(label).when(repo).save(labelWithoutId);
+//        when(service.getAllProduce()).thenReturn(produceList);
+    }
+
+    @Test
+    public void getAllLabelShouldReturnAListAnd200() throws Exception {
+        // Arrange
+        Label label = new Label(1,1,1,true);
+        List<Label> labelList = Arrays.asList(label);
+
+        String expectedJsonValue = mapper.writeValueAsString(labelList);
+        // Act
+        mockMvc.perform(get("/label"))
+                .andDo(print())
+                .andExpect(status().isOk())                 // Assert
+                .andExpect(content().json(expectedJsonValue));
+    }
+
+    @Test
+    public void createLabelShouldReturnNewLabelAndStatus201() throws Exception {
+        // Arrange
+        Label outputLabel = new Label(1,1,1,true);
+        Label inputLabel = new Label(1,1,true);
+
+        String outputLabelJson = mapper.writeValueAsString(outputLabel);
+        String inputLabelJson = mapper.writeValueAsString(inputLabel);
+
+        // Act
+        mockMvc.perform(post("/label")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(inputLabelJson))
+                .andDo(print())
+                .andExpect(status().isCreated())            // Assert
+                .andExpect(content().json(outputLabelJson));  // Assert
+    }
+
+    @Test
+    public void getLabelByIdShouldReturnStatus200() throws Exception {
+        Label outputLabel = new Label(1,1,1,true);
+        String outputLabelJson = mapper.writeValueAsString(outputLabel);
+        doReturn(Optional.of(outputLabel)).when(repo).findById(1);
+        mockMvc.perform(get("/label/1"))
+                .andExpect(status().isOk())
+                .andExpect((content().json(outputLabelJson)));
+    }
+
+
+    @Test
+    public void updateLabelShouldReturnStatus200() throws Exception {
+        Label InputLabel = new Label(1,1,1,true);
+        String expectedLabelJson = mapper.writeValueAsString(InputLabel);
+        mockMvc.perform(put("/label/1")
+                        .content(expectedLabelJson)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isNoContent());
+    }
+
+
+    @Test
+    public void DeleteLabelByIdShouldReturnStatus204() throws Exception {
+        mockMvc.perform(delete("/label/2"))
+                .andDo(print())          // Assert
+                .andExpect(status().isNoContent());  // Assert
+
+
+    }
 }
